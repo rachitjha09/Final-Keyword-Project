c4645936fd30cc1f030a53ef4d8b57e01b491bef
rachitjha09 <rachitjha09@gmail.com>
2024-09-02 19:21:53 -0400

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,21 @@
+
+/**
+ * Practicing the final keyword.
+ *
+ * @author (Rachit Jha)
+ * @version (9/15/22)
+ */
+public class Demo
+{
+    public static void main(String[]args){
+        int n=7;
+        final int m=4;
+        System.out.println(n);
+        //m=6;
+        //another example:
+        final int z;
+        z=4;
+        final double PI=3.14159265358979;
+        System.out.println(PI);
+    }
+}
